5aa5ac31d2b63675429f12c4336099a21b4059b0
Kenneth Murerwa <xwaxes@gmail.com>
2023-03-02 20:03:48 +0300

    chore: Clean up code and remove unnecessary views

--- app/src/main/java/com/murerwa/reusableadapter/FirstFragment.kt
+++ app/src/main/java/com/murerwa/reusableadapter/FirstFragment.kt
@@ -1,40 +1,23 @@
 package com.murerwa.reusableadapter
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.murerwa.reusableadapter.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
-
+class FirstFragment : Fragment(R.layout.fragment_first) {
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
-
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentFirstBinding.bind(view)
     }
 
     override fun onDestroyView() {
-        super.onDestroyView()
         _binding = null
+
+        super.onDestroyView()
     }
 }
